70bd5aed8506248636dae0acfbb629707cf91d92
Alwin Doss <alwindoss84@gmail.com>
2023-10-18 23:01:45 +0530

    add mui to create ui components
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

---
+++ frontend/src/components/ButtonUsage.jsx
@@ -0,0 +1,6 @@
+import Button from '@mui/material/Button';
+import styles from './ButtonUsage.module.css';
+
+export default function ButtonUsage() {
+    return <Button variant="contained" className={styles.button_usage}>Hello world</Button>;
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './style.css'
 import App from './App'
 
@@ -9,6 +14,6 @@ const root = createRoot(container)
 
 root.render(
     <React.StrictMode>
-        <App/>
+        <App />
     </React.StrictMode>
 )
